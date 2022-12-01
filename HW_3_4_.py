158773e24a0e333ab7e9e61c1b3849007a03f550
imr666 <118901326+imr666@users.noreply.github.com>
2022-12-01 12:14:16 +0300

    ДЗ

---
+++ HW_3_4_.py
@@ -0,0 +1,16 @@
+def open_browser():
+    browser_name = 'Open Browser Chrome'
+    return browser_name
+print(open_browser())
+
+def go_to_companyname_homepage():
+    page_url = 'Go To Companyname Homepage Google.com'
+    return page_url
+print(go_to_companyname_homepage())
+
+def find_registration_button_on_login_page():
+    page_url = 'Find Registration Button On Login Page Google.com'
+    button_text = 'Login'
+    return page_url + ' --> ' + button_text
+print(find_registration_button_on_login_page())
+
